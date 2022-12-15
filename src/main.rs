b063f0fd5472453d0796517aa79d763bb3a8b306
Benjamin Shapiro <bshap93@gmail.com>
2022-12-14 18:43:11 -0700

    Rudimentary combat and message based ecs functionality

--- src/main.rs
+++ src/main.rs
@@ -56,7 +56,6 @@ impl State {
             input_systems: build_input_scheduler(),
             player_systems: build_player_scheduler(),
             monster_systems: build_monster_scheduler()
-            //TODO Left off Here - Pg. 134/147 close top
         }
     }
 }
@@ -70,9 +69,20 @@ impl GameState for State {
         ctx.set_active_console(1);
         ctx.cls();
         self.resources.insert(ctx.key);
-        self.systems.execute(&mut self.ecs, &mut self.resources);
+        let current_state = self.resources.get::<TurnState>().unwrap().clone();
+        match current_state {
+            TurnState::AwaitingInput => self.input_systems.execute(
+               &mut self.ecs,
+               &mut self.resources 
+            ),
+            TurnState::PlayerTurn => {
+                self.player_systems.execute(&mut self.ecs, &mut self.resources);
+            },
+            TurnState::MonsterTurn => {
+                self.monster_systems.execute(&mut self.ecs, &mut self.resources);
+            }
+        }
         render_draw_buffer(ctx).expect("Render error");
-
     }
 }
 
